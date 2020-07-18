442b6d85f2f3836414a54b7248cbff8b80508259
Amit-Agarwal01 <60521717+Amit-Agarwal01@users.noreply.github.com>
2020-07-19 00:17:17 +0530

    Update natoursLWC.js

--- natoursLWC/natoursLWC.js
+++ natoursLWC/natoursLWC.js
@@ -1,14 +1,11 @@
-import { LightningElement,api } from 'lwc';
+import { LightningElement } from 'lwc';
 import Natour_Image from '@salesforce/resourceUrl/img';
    
 export default class NatoursLWC extends LightningElement {
-    @api isTrue;
+    
     Natimage1 = Natour_Image + '/nat-1.png';
     Natimage3 = Natour_Image + '/nat-3.png';
     Natimage2 = Natour_Image + '/nat-2.png';
 
-     
-    handleClick() {
-      this.dispatchEvent(new CustomEvent(true));
-    }
+
 }
